d3736fbc5a31c52622f0d5030a1b5240222fc6bd
Ahmet Said <asaidozdemir@gmail.com>
2022-11-30 08:12:59 +0300

    Make move

---
+++ engine.py
@@ -0,0 +1,97 @@
+import random
+
+
+class Ship:
+    def __init__(self, size):
+        self.row = random.randrange(0, 9)
+        self.col = random.randrange(0, 9)
+        self.size = size
+        self.orientation = random.choice(["h", "v"])
+        self.indexes = self.compute_indexes()
+
+    def compute_indexes(self):
+        start_index = self.row * 10 + self.col
+        if self.orientation == "h":
+            return [start_index + i for i in range(self.size)]
+        elif self.orientation == "v":
+            return [start_index + i * 10 for i in range(self.size)]
+
+
+class Player:
+    def __init__(self):
+        self.ships = []
+        self.search = ["U" for i in range(100)]  # "U" for "unknown"
+        self.place_ships(sizes=[5, 4, 3, 3, 2])
+        list_of_lists = [ship.indexes for ship in self.ships]
+        self.indexes = [index for sublist in list_of_lists for index in sublist]
+
+    def place_ships(self, sizes):
+        for size in sizes:
+            placed = False
+            while not placed:
+
+                # create a new ship
+                ship = Ship(size)
+
+                # check if placement is valid
+                possible = True
+                for i in ship.indexes:
+
+                    # indexes must be <100
+                    if i >= 100:
+                        possible = False
+                        break
+
+                    # ships must not overflow to next row
+                    new_row = i // 10
+                    new_col = i % 10
+                    if new_row != ship.row and new_col != ship.col:
+                        possible = False
+                        break
+
+                    # ships must not collide
+                    for other_ship in self.ships:
+                        if i in other_ship.indexes:
+                            possible = False
+                            break
+
+                # place the ship
+                if possible:
+                    self.ships.append(ship)
+                    placed = True
+
+    def show_ships(self):
+        indexes = ["-" if i not in self.indexes else "X" for i in range(100)]
+        for row in range(1, 11):
+            print(" ".join(indexes[(row - 1) * 10:row * 10]))
+        print()
+
+
+class Game:
+    def __init__(self):
+        self.player1 = Player()
+        self.player2 = Player()
+        self.player1_turn = True
+        self.over = False
+
+    def make_move(self, i):
+        player = self.player1 if self.player1_turn else self.player2
+        opponent = self.player2 if self.player1_turn else self.player1
+
+        # miss "M or hit "H"
+        if i in opponent.indexes:
+            player.search[i] = "H"
+
+            # check if ship is sunk "S"
+            for ship in opponent.ships:
+                sunk = True
+                for i in ship.indexes:
+                    if player.search[i] == "U":
+                        sunk = False
+                        break
+                if sunk:
+                    for i in ship.indexes:
+                        player.search[i] = "S"
+
+        else:
+            player.search[i] = "M"
--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+from engine import Player
+
 # setting up pygame
 import pygame
 
@@ -10,23 +12,55 @@ SQ_SIZE = 45
 H_MARGIN = SQ_SIZE * 4
 V_MARGIN = SQ_SIZE
 
+INDENT = 10
+
 WIDTH = SQ_SIZE * 10 * 2 + H_MARGIN
 HEIGHT = SQ_SIZE * 10 * 2 + V_MARGIN
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # colors
 GREY = (40, 50, 60)
+GREEN = (50, 200, 150)
 WHITE = (250, 250, 250)
+BLUE = (50, 150, 200)
+ORANGE = (250, 140, 20)
+RED = (250, 50, 100)
+COLORS = {"U": GREY, "M": BLUE, "H": ORANGE, "S": RED}
 
 
 # grid
-def draw_grid(left=0, top=0):
+def draw_grid(player, left=0, top=0, search=False):
     for i in range(100):
         x = left + i % 10 * SQ_SIZE
         y = top + i // 10 * SQ_SIZE
         square = pygame.Rect(x, y, SQ_SIZE, SQ_SIZE)
         pygame.draw.rect(SCREEN, WHITE, square, width=3)
+        if search:
+            x += SQ_SIZE // 2
+            y += SQ_SIZE // 2
+            pygame.draw.circle(SCREEN, COLORS[player.search[i]], (x, y), radius=SQ_SIZE // 4)
 
+
+# function to draw ships onto the position grids
+def draw_ships(player, left=0, top=0):
+    for ship in player.ships:
+        x = left + ship.col * SQ_SIZE + INDENT
+        y = top + ship.row * SQ_SIZE + INDENT
+        if ship.orientation == "h":
+            width = ship.size * SQ_SIZE - 2 * INDENT
+            height = SQ_SIZE - 2 * INDENT
+        else:
+            width = SQ_SIZE - 2 * INDENT
+            height = ship.size * SQ_SIZE - 2 * INDENT
+        rectangle = pygame.Rect(x, y, width, height)
+        pygame.draw.rect(SCREEN, GREEN, rectangle, border_radius=INDENT)
+
+
+player1 = Player()
+player1.show_ships()
+
+player2 = Player()
+player2.show_ships()
 
 # pygame loop
 animating = True
@@ -57,12 +91,16 @@ while animating:
         SCREEN.fill(GREY)
 
         # draw search grids
-        draw_grid()
-        draw_grid(left=(WIDTH-H_MARGIN)//2+H_MARGIN)
+        draw_grid(player1, search=True)
+        draw_grid(player2, search=True, left=(WIDTH - H_MARGIN) // 2 + H_MARGIN)
 
         # draw position grids
-        draw_grid(top=(HEIGHT-V_MARGIN)//2+V_MARGIN)
-        draw_grid(left=(WIDTH-H_MARGIN)//2+H_MARGIN,top=(HEIGHT-V_MARGIN)//2+V_MARGIN)
+        draw_grid(player1, top=(HEIGHT - V_MARGIN) // 2 + V_MARGIN)
+        draw_grid(player2, left=(WIDTH - H_MARGIN) // 2 + H_MARGIN, top=(HEIGHT - V_MARGIN) // 2 + V_MARGIN)
+
+        # draw ships onto grids
+        draw_ships(player1, top=(HEIGHT - V_MARGIN) // 2 + V_MARGIN)
+        draw_ships(player2, left=(WIDTH - H_MARGIN) // 2 + H_MARGIN)
 
         # update window
         pygame.display.flip()
